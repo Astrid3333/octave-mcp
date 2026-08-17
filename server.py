073fa9773b47f545545cd87211a530da4e467e4f
Astrid3333 <desastrid.belen@gmail.com>
2026-08-17 03:15:32 +0000

    server.py: registra import de tool_catalog_tool

--- server.py
+++ server.py
@@ -109,6 +109,7 @@ import solar_radiation_tool  # auto-registra via tool_registry, no requiere mas 
 import wind_power_curve_tool  # auto-registra via tool_registry, no requiere mas ediciones
 import battery_sizing_tool  # auto-registra via tool_registry, no requiere mas ediciones
 import renewable_mpc_controller  # auto-registra via tool_registry, no requiere mas ediciones
+import tool_catalog_tool  # auto-registra via tool_registry, no requiere mas ediciones
 from archaeoastronomy_tool import compute_archaeoastronomy, ARCHAEOASTRONOMY_TOOL_SCHEMA
 from quantum_information_tool import compute_quantum_information, QUANTUM_INFORMATION_TOOL_SCHEMA
 from octave_infra_tool import octave_run, octave_eval_expr, octave_run_script, octave_version
