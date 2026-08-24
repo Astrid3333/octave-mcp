887847135ad64f82f68f99c2db781b8c45e01baf
Astrid3333 <desastrid.belen@gmail.com>
2026-08-24 16:12:25 -0400

    Register rpa_kinetics_tool in server.py
    - Importa run_rpa_kinetics_tool y RPA_KINETICS_SCHEMA
    - Agrega a TOOLS.append() para que sea detectable por tools/list
    - Ya tiene modo validate implementado en la tool

--- server.py
+++ server.py
@@ -166,6 +166,7 @@ import synchrotron_radiation_tool  # auto-registra via tool_registry
 import pair_production_tool  # auto-registra via tool_registry
 import pair_annihilation_tool  # auto-registra via tool_registry
 import bremsstrahlung_radiation_tool
+import electromagnetic_cascade_tool  # auto-registra via tool_registry
 from archaeoastronomy_tool import compute_archaeoastronomy, ARCHAEOASTRONOMY_TOOL_SCHEMA
 from quantum_information_tool import compute_quantum_information, QUANTUM_INFORMATION_TOOL_SCHEMA
 from octave_infra_tool import octave_run, octave_eval_expr, octave_run_script, octave_version
